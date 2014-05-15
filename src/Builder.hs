cb84c2beac144e22d49d97f8ae039e167ece6d5d
Callum Rogers <callumrogers@hotmail.co.uk>
2014-05-15 23:57:42 +0100

    Changed buildBasicBlock to addToBaiscBlock

--- src/Builder.hs
+++ src/Builder.hs
@@ -3,71 +3,78 @@
 module Builder where
 
 import Control.Applicative (Applicative)
+import Control.Monad (void)
 import Control.Monad.State (MonadState, State, get, put, runState)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
-import Data.Maybe (catMaybes)
+import Data.Maybe (isJust)
 import Data.Word (Word)
 
 import qualified LLVM.General.AST as LL
 import qualified LLVM.General.AST.Constant as LLC
 import qualified LLVM.General.AST.Global as LLG
 
-data FState = FState (Map LL.Name (Maybe LL.BasicBlock)) Word LL.Global
-	deriving Show
+data FState = FState (Map LL.Name (Maybe (BasicBlockBuilder ()))) LL.Global
 
 newtype FunctionBuilder a = FunctionBuilder (State FState a)
 	deriving (Functor, Monad, Applicative, MonadState FState)
 
+onlyJusts :: [(a, Maybe b)] -> [(a, b)]
+onlyJusts = map (\(a, Just b) -> (a, b)) . filter (isJust . snd)
+
 runFunctionBuilder :: FunctionBuilder a -> String -> LL.Global
-runFunctionBuilder (FunctionBuilder s) n = f { LLG.name = name, LLG.returnType = LL.IntegerType 32, LLG.basicBlocks = bbList }
-	where name = LL.Name n
-	      initialState = FState M.empty 0 LLG.functionDefaults
-	      (FState bbs _ f) = snd $ runState s initialState
-	      bbList = catMaybes $ map snd $ M.toList bbs
+runFunctionBuilder (FunctionBuilder s) nameStr = f { LLG.name = name, LLG.returnType = LL.IntegerType 32, LLG.basicBlocks = bbs }
+	where name = LL.Name nameStr
+	      initialState = FState M.empty LLG.functionDefaults
+	      (FState bbbs f) = snd $ runState s initialState
+	      g (c, bbList) (n, bbb) = let (c', bb) = runBasicBlockBuilder bbb n c in (c', bbList ++ [bb])
+	      bbs = snd $ foldl g (0, []) $ onlyJusts $ M.toList bbbs
 
 setParameters :: [(LL.Type, String)] -> FunctionBuilder ()
 setParameters ps = do
-	(FState bbs c f) <- get
+	(FState bbbs f) <- get
 	let params = map (\(t,n) -> LL.Parameter t (LL.Name n) []) ps
 	let f' = f { LLG.parameters = (params, False) }
-	put $ FState bbs c f'
+	put $ FState bbbs f'
 
 newtype BasicBlockRef = BasicBlockRef LL.Name
 
 createBasicBlockRef :: String -> FunctionBuilder BasicBlockRef
 createBasicBlockRef n = do
 	let name = LL.Name n
-	(FState bbs c f) <- get
-	if M.member name bbs then error $ "Basic block " ++ n ++ " already exists"
+	(FState bbbs f) <- get
+	if M.member name bbbs then error $ "Basic block " ++ n ++ " already exists"
 	else do
-		put $ FState (M.insert name Nothing bbs) c f
+		put $ FState (M.insert name Nothing bbbs) f
 		return $ BasicBlockRef name
 
-buildBasicBlock :: BasicBlockRef -> BasicBlockBuilder () -> FunctionBuilder ()
-buildBasicBlock (BasicBlockRef name) bbb = do
-	(FState bbs c f) <- get
-	case M.lookup name bbs of
+addToBasicBlock :: BasicBlockRef -> BasicBlockBuilder a -> FunctionBuilder ()
+addToBasicBlock (BasicBlockRef name) bbb' = do
+	(FState bbbs f) <- get
+	let bbb = void bbb'
+	case M.lookup name bbbs of
 		Nothing -> error $ "No refernce for " ++ show name
-		Just (Just _) -> error $ "Already build basic block for " ++ show name
+		Just (Just oldBBB) ->
+			let newBBB = void $ oldBBB >> bbb in
+			put $ FState (M.insert name (Just newBBB) bbbs) f
 		Just Nothing -> do
-			let (c', bb) = runBasicBlockBuilder bbb name c
-			put $ FState (M.insert name (Just bb) bbs) c' f
+			put $ FState (M.insert name (Just $ void bbb) bbbs) f
 
 basicBlock :: String -> BasicBlockBuilder () -> FunctionBuilder ()
-basicBlock n bbb = createBasicBlockRef n >>= flip buildBasicBlock bbb
+basicBlock n bbb = createBasicBlockRef n >>= flip addToBasicBlock bbb
 
 ftest :: FunctionBuilder ()
 ftest = do
 	cat <- createBasicBlockRef "cat"
 	hat <- createBasicBlockRef "hat"
-	buildBasicBlock cat $ do
+	addToBasicBlock cat $ do
 		a <- add c3 c3
 		add c3 a
-		br hat
-	buildBasicBlock hat $ do
+	addToBasicBlock hat $ do
 		a <- add c3 c3
 		ret a
+	addToBasicBlock cat $ do
+		ret c3
 
 
 data BState = BState [LL.Named LL.Instruction] (Maybe (LL.Named LL.Terminator)) Word
