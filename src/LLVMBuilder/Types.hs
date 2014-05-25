a96b96c224057fa8389f647b62a7bfb8a1d33652
Callum Rogers <callumrogers@hotmail.co.uk>
2014-05-25 23:01:44 +0100

    Started writing singletons

--- src/LLVMBuilder/Types.hs
+++ src/LLVMBuilder/Types.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE TemplateHaskell, GADTs, DataKinds, ConstraintKinds, TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell, GADTs, DataKinds, TypeFamilies #-}
 
 module LLVMBuilder.Types where
 
 import Control.Lens
 
 import qualified LLVM.General.AST as LL
+import LLVM.General.AST.Type (FloatingPointFormat)
+
+import Data.Singletons.TH
+--import Data.Singletons.TypeLits
 
 newtype BasicBlockRef = BasicBlockRef { _unBasicBlockRef :: LL.Name } deriving (Eq, Show)
 makeLenses ''BasicBlockRef
@@ -12,3 +16,17 @@ makeLenses ''BasicBlockRef
 newtype ValueRef = ValueRef { _unValueRef :: LL.Operand } deriving (Eq, Show)
 makeLenses ''ValueRef
 
+genSingletons [''FloatingPointFormat]
+
+singletons [d|
+	data Nat = Zero | Succ Nat
+	data LLVMType
+		= VoidTy
+		| IntTy Nat
+		| PointerTy LLVMType
+		| FloatTy Nat FloatingPointFormat
+		| FunctionTy LLVMType [LLVMType] Bool
+		| VectorTy Nat LLVMType
+		| StructTy Nat [LLVMType]
+		| ArrayTy Nat LLVMType
+ |]
