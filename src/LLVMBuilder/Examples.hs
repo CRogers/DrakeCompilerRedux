305c36a76169636a0b44c6deaeb192e3f0330046
Callum Rogers <callumrogers@hotmail.co.uk>
2014-05-26 16:49:19 +0100

    Update example to check everything actually works

--- src/LLVMBuilder/Examples.hs
+++ src/LLVMBuilder/Examples.hs
@@ -1,19 +1,20 @@
-{-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE RebindableSyntax, DataKinds #-}
 
-module Examples where
+module LLVMBuilder.Examples where
 
 import Prelude hiding (Monad(..))
 import IxMonadSyntax
 
 import LLVMBuilder.Builder
 
-c3 :: ValueRef
-c3 = constant 3
+c3 :: ValueRef ('IntTy N32)
+c3 = constant sn32 3
 
 test :: Builder Setup Terminated ()
 test = do
 	entry <- createBasicBlock "entry"
 	switchTo entry
+	alloca (SIntTy sn32) $ constant sn32 1
 	b <- add c3 c3
 	x <- add b b
 	ret x
