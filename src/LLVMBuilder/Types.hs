f33712817e549cfe6b83de6402e9071ce9be69c8
Callum Rogers <callumrogers@hotmail.co.uk>
2014-05-26 15:23:32 +0100

    Singletons fun

--- src/LLVMBuilder/Types.hs
+++ src/LLVMBuilder/Types.hs
@@ -1,32 +1,56 @@
-{-# LANGUAGE ScopedTypeVariables, TemplateHaskell, GADTs, DataKinds, TypeFamilies #-}
+{-# LANGUAGE StandaloneDeriving, FlexibleInstances, ScopedTypeVariables, TypeOperators, UndecidableInstances, TemplateHaskell, GADTs, DataKinds, ConstraintKinds, TypeFamilies #-}
+--{-# OPTIONS_GHC -ddump-splices #-}
 
 module LLVMBuilder.Types where
-
-import Control.Lens
 
 import qualified LLVM.General.AST as LL
 import LLVM.General.AST.Type (FloatingPointFormat)
 
 import Data.Singletons.TH
---import Data.Singletons.TypeLits
 
-newtype BasicBlockRef = BasicBlockRef { _unBasicBlockRef :: LL.Name } deriving (Eq, Show)
-makeLenses ''BasicBlockRef
+newtype BasicBlockRef = BasicBlockRef { unBasicBlockRef :: LL.Name } deriving (Eq, Show)
 
-newtype ValueRef = ValueRef { _unValueRef :: LL.Operand } deriving (Eq, Show)
-makeLenses ''ValueRef
-
-genSingletons [''FloatingPointFormat]
+--genSingletons [''FloatingPointFormat]
 
 singletons [d|
-	data Nat = Zero | Succ Nat
+	data Nat = Zero | Succ Nat deriving (Eq, Show)
+
 	data LLVMType
-		= VoidTy
+		= VoidTy Nat
 		| IntTy Nat
 		| PointerTy LLVMType
-		| FloatTy Nat FloatingPointFormat
+		{-| FloatTy Nat --FloatingPointFormat
 		| FunctionTy LLVMType [LLVMType] Bool
 		| VectorTy Nat LLVMType
 		| StructTy Nat [LLVMType]
-		| ArrayTy Nat LLVMType
+		| ArrayTy Nat LLVMType-}
+		deriving (Show)
  |]
+
+data ValueRef :: LLVMType -> * where
+	ValueRef :: LL.Operand -> SLLVMType t -> ValueRef t
+
+instance Show (ValueRef t) where
+	show (ValueRef op ty) = "ValueRef (" ++ show op ++ ") (" ++ show (fromSing ty) ++ ")"
+
+type P1 n = Succ n
+type P2 n = P1 (P1 n)
+type P3 n = P2 (P2 n)
+type P4 n = P3 (P3 n)
+type P5 n = P4 (P4 n)
+
+type N32 = P5 Zero
+type SN32 = Sing N32
+
+n1 n = SSucc n
+n2 n = n1 (n1 n)
+n3 n = n2 (n2 n)
+n4 n = n3 (n3 n)
+n5 n = n4 (n4 n)
+
+sn32 :: SN32
+sn32 = n5 SZero
+
+toNat :: Integer -> Nat
+toNat 0 = Zero
+toNat n = Succ (toNat $ n - 1)
